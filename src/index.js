1cf29fbcf3a529d0df155e61e8c7e9942b402ec6
heru-nan <hrwgallardo@gmail.com>
2020-10-10 20:57:07 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { render } from "react-dom";
+import React from "react";
+import "./styles.css"
+
+function App() {
+  return <div><h2>Build With Parcel</h2></div>;
+}
+
+render(<App />, document.getElementById("App"));
